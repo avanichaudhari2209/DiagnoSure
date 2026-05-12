de8cda41f7899032dadde08a7ad141e16c25d1de
avanichaudhari2209 <avaniac06@gmail.com>
2026-05-12 14:38:43 +0530

    Clean up settings.py by removing sensitive info
    Removed default secret key and unnecessary comments.

--- DiagnoSure-main/diagnosure/settings.py
+++ DiagnoSure-main/diagnosure/settings.py
@@ -16,13 +16,8 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
+SECRET_KEY = "secret-key"
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-m_xe4au-g9$%h6j&o4p2b13ki#tm+mg(^ac*ytwc*r*q^bfqyo"
-
-# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 ALLOWED_HOSTS = []
@@ -102,10 +97,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
-# Internationalization
-# https://docs.djangoproject.com/en/6.0/topics/i18n/
-
 LANGUAGE_CODE = "en-us"
 
 TIME_ZONE = "UTC"
@@ -114,9 +105,6 @@ USE_I18N = True
 
 USE_TZ = True
 
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = "static/"
 
